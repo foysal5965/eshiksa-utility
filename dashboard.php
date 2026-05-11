111c05f15752a03106d97790793aa5845e4d2f23
Your Name <foysalahmed5965@gmail.com>
2026-05-11 19:38:42 +0600

    jdfsdf

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'includes/header.php';
 check_login(); // This function protects the page
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <h2>Dashboard</h2>
